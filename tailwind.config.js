ef9ca027c9c627e1ae44ee79ad2782c5fc1375cb
Fajar Abdurrohman <48176273+fajar2510@users.noreply.github.com>
2023-04-15 09:46:50 +0700

    grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,30 @@
 /** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      spacing: {
+        13: "3.25rem",
+      },
+      fontFamily: {
+        inter: ["Inter"],
+      },
+      colors: {
+        warnaku: "#FFFFFF", // ini adalah custom colors
+      },
+      animation: {
+        "spin-slow": "spin 3s linear infinite",
+        "bounce-slow": "bounce 3s ease-in-out infinite",
+        goyang: "goyang 1s ease-in-out infinite",
+      },
+      keyframes: {
+        goyang: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
